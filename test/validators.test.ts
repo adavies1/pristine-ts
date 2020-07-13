4683493f273ce8f01f983041d90b76df5487045c
Alex Davies <alexdavieswebdev@gmail.com>
2020-07-13 19:08:31 +0100

    [Tests] - Added tests for 'required' validator

--- test/validators.test.ts
+++ test/validators.test.ts
@@ -8,4 +8,15 @@ describe('Validators', () => {
             expect(validators.text('123 456 abc def')).toBe(true);
         });
     });
+
+    describe('required', () => {
+        test('it should return true when a value is present', () => {
+            expect(validators.required('123')).toBe(true);
+            expect(validators.required('123 456 abc def')).toBe(true);
+        });
+
+        test('it should return false if a value is not present', () => {
+            expect(validators.required('')).toBe(false);
+        });
+    });
 });

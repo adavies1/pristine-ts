96980788631857c94407c58c367f0defbe8e3d08
Alex Davies <alexdavieswebdev@gmail.com>
2020-07-13 19:10:16 +0100

    [Tests] - Added tests for 'email' validator

--- test/validators.test.ts
+++ test/validators.test.ts
@@ -19,4 +19,27 @@ describe('Validators', () => {
             expect(validators.required('')).toBe(false);
         });
     });
+
+    describe('email', () => {
+        test('it should return true when a value is not present', () => {
+            expect(validators.email('')).toBe(true);
+        });
+
+        test('it should return true when a valid email address is passed', () => {
+            expect(validators.email('hello@gmail.com')).toBe(true);
+            expect(validators.email('hello-there@gmail.com')).toBe(true);
+            expect(validators.email('my.email@gmail.com')).toBe(true);
+            expect(validators.email('my.email123@gmail.com')).toBe(true);
+            expect(validators.email('£mail123!#$%&\'*+-/=?^_`{|}~hfghf@gmail.com')).toBe(true);
+            expect(validators.email('email+label@gmail.net')).toBe(true);
+        });
+
+        test('it should return false when an invalid email address is passed', () => {
+            expect(validators.email('hgdfhgdfhfhf')).toBe(false);
+            expect(validators.email('frgdfg*&%%$££!"()')).toBe(false);
+            expect(validators.email('hello@gmail')).toBe(false);
+            expect(validators.email('my.email@gmail.com^')).toBe(false);
+            expect(validators.email('email+label()@gmail.com')).toBe(false);
+        });
+    });
 });

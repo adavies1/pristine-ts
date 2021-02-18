fa65ba1ec97d982921d02c91bfa261e89b206f38
Alex Davies <alexdavieswebdev@gmail.com>
2021-02-18 22:35:04 +0000

    [Config] - Major overhaul of configs which should sort out build settings

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -1,42 +1,18 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = [
-    {
-        entry: {
-            pristine: './src/Pristine.ts'
-        },
-        mode: 'development',
-        target: 'web',
-        module: {
-            rules: [
-                {
-                    test: /\.ts?$/,
-                    use: {
-                        loader: 'ts-loader',
-                    },
-                    exclude: /node_modules/,
-                }
-            ]
-        },
-        output: {
-            filename: '[name].js',
-            path: path.resolve(__dirname, 'dist'),
-            library: 'Pristine',
-            libraryTarget: 'umd',
-            libraryExport: 'default'
-        },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js', '.jsx']
-        },
-        devServer: {
-            publicPath: '/dist/',
-            hot: false,
-            inline: false,
-            injectClient: false,
-            injectHot: false,
-            liveReload: false
-        },
-        devtool: 'inline-source-map'
-    }
-];
+const defaultConfig = require('./webpack.config.js');
+
+module.exports = {
+    ...defaultConfig,
+    mode: 'development',
+    devServer: {
+        publicPath: '/dist/',
+        hot: false,
+        inline: false,
+        injectClient: false,
+        injectHot: false,
+        liveReload: false
+    },
+    devtool: 'inline-source-map'
+};
--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,24 @@ module.exports = {
         pristine: './src/Pristine.ts'
     },
     mode: 'production',
+    target: 'web',
     module: {
         rules: [
             {
                 test: /\.ts?$/,
                 use: {
                     loader: 'ts-loader',
+                    options: {
+                        configFile: "tsconfig.bundle.json"
+                    }
                 },
-                exclude: /node_modules/,
+                exclude: [/node_modules/, /.test.ts/]
             }
         ]
     },
     output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist/bundle'),
         library: 'Pristine',
         libraryTarget: 'umd',
         libraryExport: 'default'
